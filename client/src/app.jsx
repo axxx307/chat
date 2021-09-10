bf9fe3a82c6e3c0549e8ebf9125eb1909b66b5e5
Mark <mark.pfnov@equitybee.com>
2021-09-10 14:22:30 +0300

    Finish window

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,45 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import "./app.scss";
-import Chats from "./components/chats";
+import ChatMenu from "./components/chat-menu";
 import ChatWindow from "./components/chat-window/chat-window";
-import { getChatMessages, getChats } from "./services/requests";
+import { getChatMessages, getChats, getUsers } from "./services/requests";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-  }
+  },
 }));
 
 const App = () => {
   const classes = useStyles();
+  const [currentUserId, setCurrentUserId] = useState(1);
+  const [users, setUsers] = useState(null);
   const [chats, setChats] = useState([]);
   const [messages, setMessages] = useState([]);
   const [selectedChat, setSelectedChat] = useState(null);
+  const [selectedMessages, setSelectedMessages] = useState([]);
 
   const selectChat = (chatId) => {
-      setSelectedChat(chatId)
-  }
+    const chat = chats.find((chat) => chat.id === chatId);
+    setSelectedChat(chat);
+    setSelectedMessages(messages.find((message) => message.chatId === chatId));
+  };
 
   useEffect(() => {
-    const getUserChats = async () => {
-      const chats = await getChats('1');
+    const getData = async () => {
+      const users = await getUsers(currentUserId);
+      setUsers(users.data);
+
+      const chats = await getChats("1");
       setChats(chats.data || []);
+
       const messagesRequest = chats.data.map(async (chat) => ({
         chatId: chat.id,
-        messages: (await getChatMessages(chat.id)).data
+        messages: (await getChatMessages(chat.id)).data,
       }));
       const messages = await Promise.all(messagesRequest);
-      console.log(messages)
       setMessages(messages || []);
-    }
+    };
 
-    getUserChats()
-  }, []);
+    getData();
+  }, [currentUserId]);
 
   return (
     <div className={classes.root}>
-     <Chats chats={chats} messages={messages} selectChat={selectChat} />
-     <ChatWindow/>
+      <ChatMenu chats={chats} messages={messages} selectChat={selectChat} />
+      <ChatWindow
+        selectedChat={selectedChat}
+        messages={selectedMessages}
+        users={users}
+        currentUserId={currentUserId}
+      />
     </div>
   );
 };
