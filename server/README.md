87e00eea9c6674e1fc9a83848e9e6b5c0f96b800
Mark <mark.pfnov@equitybee.com>
2021-09-10 12:38:57 +0300

    Add basic socket functionality

---
+++ server/README.md
@@ -0,0 +1,40 @@
+
+# Running the server
+Type "npm start" in the terminal
+
+# DB schemas:
+
+## User
+
+```
+{
+	"id": string;
+	"userName": string;
+	"password": string;
+	"chatIds": number[];
+}
+```
+
+## Chat
+```
+{
+	"id": string;
+	"name": string;
+	"userIds": string[]
+	"createdAt": number;
+	"messageIds": string[];
+}
+```
+
+## Message
+```
+{
+	"id": string;
+	"chatId": string;
+	"fromUserId": string;
+	"toUserId": string;
+	"createdAt": number;
+	"status": string; // sent, delivered, read
+	"text": string
+}
+```
--- server/src/chat.js
+++ server/src/chat.js
@@ -1,5 +1,32 @@
-function Chat(io, db) {
+const uuidv4 = require('uuid').v4;
 
+const users = new Map();
+function Connect(io, socket, db) {
+  console.log('connect');
+  const sendMessage = (value) => io.sockets.emit('message', value);
+  const saveMessage = async (value) => {
+    const { text, fromUserId, toUserId } = value;
+    const message = {
+      chatId: 'r',
+      text,
+      toUserId,
+      fromUserId,
+      id: uuidv4(),
+      createdAt: Date.now(),
+      status: 'sent',
+    };
+
+    await db.get('messages').push(message).write();
+    sendMessage(message);
+  };
+
+  socket.on('message', (value) => {
+    saveMessage(value);
+  });
+}
+
+function Chat(io, db) {
+  io.on('connection', (socket) => new Connect(io, socket, db));
 }
 
 module.exports = Chat;
